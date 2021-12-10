01c8b385d4bcbee3646b0fae270c2c18d388f556
Scott Brown <link1900@gmail.com>
2021-12-11 10:24:22 +1100

    build(node-graphql): Added EmailType

--- packages/node-graphql/src/index.ts
+++ packages/node-graphql/src/index.ts
@@ -3,5 +3,6 @@ export * from "./client/graphqlClientHelper";
 export * from "./client/ExternalGraphqlApiError";
 
 export * from "./graphql-types/DateTimeInZoneType";
+export * from "./graphql-types/EmailType";
 export * from "./graphql-types/PlainDateType";
 export * from "./graphql-types/TimezoneGraphqlType";
